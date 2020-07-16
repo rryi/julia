bfd980a5b6ec2c068cbf3fb79782838496e01b50
Jeff Bezanson <jeff.bezanson@gmail.com>
2020-07-15 21:58:06 -0400

    fix some issues with color configuration
    - `--color=no` did not remove all color in the REPL
    - color was not fully disabled by default when stdout was a pipe
    - `--color=yes` did not enable color when stdout was a pipe (fixes #30703)

--- base/client.jl
+++ base/client.jl
@@ -210,6 +210,14 @@ function exec_options(opts)
     history_file          = (opts.historyfile != 0)
     color_set             = (opts.color != 0) # --color!=auto
     global have_color     = color_set ? (opts.color == 1) : nothing # --color=on
+    if color_set
+        if !isa(stdout, TTY)
+            global stdout = IOContext(stdout, :color => have_color::Bool)
+        end
+        if !isa(stderr, TTY)
+            global stderr = IOContext(stderr, :color => have_color::Bool)
+        end
+    end
     global is_interactive = (opts.isinteractive != 0)
 
     # pre-process command line argument list
@@ -371,13 +379,12 @@ function run_main_repl(interactive::Bool, quiet::Bool, banner::Bool, history_fil
         invokelatest(REPL_MODULE_REF[]) do REPL
             term_env = get(ENV, "TERM", @static Sys.iswindows() ? "" : "dumb")
             term = REPL.Terminals.TTYTerminal(term_env, stdin, stdout, stderr)
-            color_set || (global have_color = REPL.Terminals.hascolor(term))
             banner && Base.banner(term)
             if term.term_type == "dumb"
                 active_repl = REPL.BasicREPL(term)
                 quiet || @warn "Terminal not fully functional"
             else
-                active_repl = REPL.LineEditREPL(term, have_color, true)
+                active_repl = REPL.LineEditREPL(term, get(stdout, :color, false), true)
                 active_repl.history_file = history_file
             end
             # Make sure any displays pushed in .julia/config/startup.jl ends up above the
@@ -487,7 +494,7 @@ function _start()
         invokelatest(display_error, catch_stack())
         exit(1)
     end
-    if is_interactive && have_color === true
+    if is_interactive && get(stdout, :color, false)
         print(color_normal)
     end
 end
